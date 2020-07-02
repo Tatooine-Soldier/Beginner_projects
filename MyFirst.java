28cf28e3de7fca1ae394d6821ce24e788b4b74c8
Tatooine-Soldier <58118455+Tatooine-Soldier@users.noreply.github.com>
2020-07-02 16:56:34 +0100

    Add files via upload
    My first attempts at learning java

---
+++ MyFirst.java
@@ -0,0 +1,85 @@
+public class MyFirst {
+  static void myMethod(int age) {
+	System.out.println(age);
+  }
+
+  public static void main(String[] args) {
+    /*String name = "T\nom";
+    System.out.println(name);
+    int myNum = 25;
+    System.out.println(myNum);
+    int one = 1;
+    int three = 3;
+    System.out.println(one + three);
+    String firstName = "John ";
+    String lastName = "Connors";
+    System.out.println("Hello " + firstName + lastName);
+    System.out.println(one % three);
+    System.out.println(one / three);
+    double newDouble = myNum;
+    System.out.println(newDouble);
+    int myNewNum = (int) newDouble;
+    System.out.println(myNewNum);
+    System.out.println(three + name);
+    String mmilk = "milk";
+    System.out.println("The Length of milk is " + mmilk.length() + " letters");
+    System.out.println(mmilk.toLowerCase());
+    System.out.println(mmilk.toUpperCase());
+    System.out.println(mmilk.indexOf("i"));
+    System.out.println(mmilk.indexOf("lk"));
+    System.out.println(firstName + " " + lastName);
+    System.out.println(firstName.concat(lastName));
+    System.out.println(name);
+    System.out.println(Math.max(5, 10));
+    System.out.println(Math.min(5, 10));
+    System.out.println(Math.abs(4.5));
+    System.out.println(Math.sqrt(81));
+    System.out.println(Math.random());
+    int randNum = (int)(Math.random() * 101);
+    System.out.println(randNum);
+    int choice_one = 10;
+    int choice_two = 20;
+    if (choice_one < choice_two) {
+	System.out.println("Choice one is smaller");
+    } else {
+	System.out.println("Choice two is bigger");
+    }
+    
+    String dresult = (choice_one < 90) ? "yes" : "no";
+    System.out.println(dresult);
+    int i = 0;
+    while (i < 5) {
+	System.out.println(i);
+	i++;
+    }
+    for (int j = 0; j < 5; j++) {
+	System.out.println(j);
+    }
+
+    String[] names = {"Jack", "David", "James", "Fintan"};
+    for (int a = 0; a < names.length; a++) {
+	System.out.println(names[a]);    
+    }
+    for (String k : names) {
+	System.out.println(k);
+    }
+
+    int[][] listz = { {1,2,3,4}, {6,8,9} };
+    int six = listz[1][0];
+    System.out.println(six);
+
+  //Questions w3 //
+
+    String q_one = "Hello\nAlexandra Abramov";
+    System.out.println(q_one);
+
+    int sum_one = 36;
+    int sum_two = 74;
+    System.out.println(sum_one + sum_two);
+    System.out.println(50/3);*/
+    
+    myMethod(20);
+    
+
+  }
+}
