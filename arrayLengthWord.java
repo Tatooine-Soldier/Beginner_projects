ab7e8e0f5e78b899c1b848f761febeeb4e2c49a1
Tatooine-Soldier <58118455+Tatooine-Soldier@users.noreply.github.com>
2020-08-14 10:51:38 +0100

    Create arrayLengthWord.java
    creates a new list of the first n elements of the original list

---
+++ arrayLengthWord.java
@@ -0,0 +1,7 @@
+public String[] wordsFront(String[] words, int n) {
+  String[] result;
+  result = new String[n];
+  for (int i = 0; i < result.length; i++) {
+    result[i] = words[i];
+  } return result;
+}
